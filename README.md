ee2742d112d81c34247779ac018a9967c0b90df2
AkiraYumoto <juanicaehd@gmail.com>
2025-09-18 16:34:11 -0300

    Agregado de funciones para el proyecto

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Challenge-Amigo-Secreto
 Proyecto de un challenge para Oracle Next Education, basado en una elección al azar después de recibir datos de entrada en un input y enviados por un button
+
+
+<h2>Descripción del proyecto</h2>
--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function ingresarNombre() {
+    nombreEntrada = document.getElementById('amigo').value
+    if (nombreEntrada == '') {
+        alert('Por favor, ingrese un nombre');
+    }else{
+        amigos.push(nombreEntrada);
+        document.getElementById('amigo').value = ''
+    }
+}
+function actualizarLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+   amigos.forEach(amigo =>{
+    lista.innerHTML = `<li>{amigo}</li>`
+   });
+}
+
+function sortearAmigos(){
+    if (amigos == ''){
+        alert("No agregaste amigos")
+    }else{
+        const amigoAleatorio = Math.floor(Math.random() * amigos.length);
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = amigos[amigoAleatorio];
+    }
+}
+
